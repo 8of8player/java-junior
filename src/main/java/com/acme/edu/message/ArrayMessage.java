eb06af76770e2c1823263a932bead62d2a402bef
dvcer <am31101990@gmail.com>
2018-03-05 01:36:28 +0200

    Merge branch '2018-02-jet' of https://github.com/8of8player/java-junior into 2018-02-jet

--- src/main/java/com/acme/edu/message/ArrayMessage.java
+++ src/main/java/com/acme/edu/message/ArrayMessage.java
@@ -1,4 +1,14 @@
 package com.acme.edu.message;
 
-public class ArrayMessage {
+public class ArrayMessage extends Message {
+    public int[] value;
+
+    public ArrayMessage(int[] message) {
+        value = message;
+    }
+
+    @Override
+    public String getEditedMsg() {
+        return editor.edit(value);
+    }
 }
